b2d57201f1687e6ed8f7e865ae8968afd54835c0
suatte <mohamedsuatte2017@gmail.com>
2025-04-26 14:38:51 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+
 function App() {
   return (
-    <div>
-      <Route path="/" component={HomePage} />
-
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to AI Resume Pro</h1>
+      <p>Your smart resume builder is ready!</p>
     </div>
   );
 }
